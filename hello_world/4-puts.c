a48a8cedaba7100662e8dc5d11cfc78cfe835a3b
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-02-22 09:47:19 +0000

    t5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,17 +1,9 @@
-#!/bin/bash
-/*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
- */
-
 #include <stdio.h>
-
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- *
- * Return: Always 0.
- */
+ *main - show characters
+ * Return: win
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
