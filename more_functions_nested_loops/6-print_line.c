2231672217b7c6c83b5520694f97c5119eca608f
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-02-28 10:06:41 +0000

    t7

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,13 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		while (a <= n)
+		while (a < n)
 		{
-			_putchar('_');
+		_putchar('_');
 			a++;
 		}
 	 _putchar('\n');
 	}
-	else
+	else if (n <= 0)
 	_putchar('\n');
 }
