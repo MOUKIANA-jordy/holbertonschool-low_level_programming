bfb79fc1b43b12e4741438914d8fe9493265f243
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-03-11 14:28:30 +0000

    k1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate a string using malloc
+ * @str: string to duplicate
+ * Return : pointer to the new string
+ */
+
+char *_strdup(char *str);
+{
+	char *a;
+	int i, j;
+
+		if (str == NULL)
+		return (NULL);
+
+		for (i = '0'; str != '\0'; i++)
+		{
+			a = malloc(i * sizeof(*a) + 1);
+			if (a == NULL)
+			{
+				return (NULL);
+			}
+			for (j = '0'; j < size; j++)
+			{
+				a[j] = str[j];
+
+				a[j] = '\0';
+			}
+		}
+		return (a);
+}
