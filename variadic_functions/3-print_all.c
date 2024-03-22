01865fa3edaf073f13bd3dc97e87392a7a848801
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-03-22 15:42:37 +0000

    t4

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,50 +1,53 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - Prints a char
- * @c: Argument pointer
- * print_float - Prints a float
- * @ap: Argument pointer
- *print_all - Prints anything
- * @format: Types of arguments passed to function
+ *  print_all - Prints anything
+ *
+ *  @format: List of args passed to function
+ *
+ *  Return: void
  */
- 
 
 void print_all(const char * const format, ...)
 {
-
-	char c;
-	int i;
-	float f;
-	char *s;
-	int format_index = 0;
+	char *str;
+	unsigned int i = 0, commaCheck = 0;
 	va_list ap;
 
-	while ((c = format[format_index++]) != '\0')
-	{
-	 if (c == 'c')
-	{
-		i = va_arg(args, int);
-		printf("%c", i);
-	} 
-		else if (c == 'i')
-		{
-			i = va_arg(args, int);
-		}	printf("%d", i); 
-		else if (c == 'f') 
-		{
-			f = va_arg(args, double);
-		}	printf("%f", f);
-		else if (c == 's') 
-		{
-			s = va_arg(args, char*);
-			if (s == NULL)
-			printf("(nil)");
-		else
-			printf("%s", s);
-		}	
-	}
+	va_start(ap, format);
 
-		va_end(args);
-		printf("\n");
+	while (format && format[i])
+	{
+		if (commaCheck)
+			printf(", ");
+
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			str = va_arg(ap, char*);
+			if (str)
+			{
+				printf("%s", str);
+				break;
+			}
+			printf("(nil)");
+			break;
+		default:
+			commaCheck = 0;
+			i++;
+			continue;
+		}
+		commaCheck = 1,	i++;
+	}
+	putchar('\n');
+	va_end(ap);
 }
