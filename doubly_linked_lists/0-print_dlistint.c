463a7499ef9a799574ac03c872d3f686f8009b0c
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-04-04 14:10:44 +0000

    ajout des commentaire 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
 
-
+/**
+ * print_dlistint - count number of nodes
+ * @h: head of double linked list
+ * Return: number of nodes, or NULL if failed
+ */
 
 
 size_t print_dlistint(const dlistint_t *h)
