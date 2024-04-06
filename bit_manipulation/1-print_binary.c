2324389b7eb929d0ca9e23805078458c7b4929a1
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-04-06 20:34:46 +0000

    ajout des commentaire de 1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 
-
-
-
-
-
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
+ */
 
 void print_binary(unsigned long int n)
 {
