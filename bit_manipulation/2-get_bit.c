5e823e9e382cdb22f77c8707224d6e314a4806c9
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-04-06 20:44:33 +0000

    2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	if (n & mask)
+	{
+		return (1);
+	}
+		return (0);
+}
