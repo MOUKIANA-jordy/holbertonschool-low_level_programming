79a463486c80211298449cb28ee070c49af293d1
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-04-06 20:31:44 +0000

    1-print_binary.c

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+
+
+
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int flag = 0;
+
+		while (mask > 0)
+		{
+			if (n & mask)
+			{
+				putchar('1');
+				flag = 1;
+			}
+			else if (flag)
+			{
+				putchar('0');
+			}
+			mask >>= 1;
+		}
+
+			if (!flag)
+			{
+			putchar('0');
+					}
+}
+
